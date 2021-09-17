397664cb887ee3d62171147dac90816ff0b69d33
mariordgez <mariordgez@hotmail.com>
2021-09-17 05:25:30 -0700

    Add basic styling to calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -18,6 +18,9 @@ class Calculator extends React.Component {
   render() {
     return (
       <div className="container">
+        <div className="gray result">
+          <p className="calc-result">0</p>
+        </div>
         <CalcButton content="AC" color="white calc-btn ac" />
         <CalcButton content="+/-" color="white calc-btn plusminus" />
         <CalcButton content="%" color="white calc-btn percent" />
@@ -36,7 +39,7 @@ class Calculator extends React.Component {
         <CalcButton content="+" color="orange calc-btn plus" />
         <CalcButton content="0" color="white calc-btn cero" />
         <CalcButton content="." color="white calc-btn dot" />
-        <CalcButton content="=" color="white calc-btn equal" />
+        <CalcButton content="=" color="orange calc-btn equal" />
       </div>
     );
   }
