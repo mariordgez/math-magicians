5865ef978ae576f1cb083c9e940bbc2e09d7b06e
mariordgez <mariordgez@hotmail.com>
2021-09-17 04:53:18 -0700

    Add calculator buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,10 +2,10 @@ import React from 'react';
 import '../App.css';
 import PropTypes from 'prop-types';
 
-function CalcButton(props) {
+function CalcButton({ color, content }) {
   return (
-    <button type="button" className={props.color}>
-      {props.content}
+    <button type="button" className={color}>
+      {content}
     </button>
   );
 }
@@ -18,14 +18,38 @@ class Calculator extends React.Component {
   render() {
     return (
       <div className="App">
+        <CalcButton content="AC" color="white" />
+        <CalcButton content="+/-" color="white" />
+        <CalcButton content="%" color="white" />
+        <CalcButton content="÷" color="orange" />
         <CalcButton content="1" color="white" />
+        <CalcButton content="2" color="white" />
+        <CalcButton content="3" color="white" />
+        <CalcButton content="x" color="orange" />
+        <CalcButton content="4" color="white" />
+        <CalcButton content="5" color="white" />
+        <CalcButton content="6" color="white" />
+        <CalcButton content="-" color="orange" />
+        <CalcButton content="7" color="white" />
+        <CalcButton content="8" color="white" />
+        <CalcButton content="9" color="white" />
+        <CalcButton content="+" color="orange" />
+        <CalcButton content="0" color="white" />
+        <CalcButton content="." color="white" />
+        <CalcButton content="=" color="white" />
       </div>
     );
   }
 }
 
-Calculator.propTypes = {
+CalcButton.propTypes = {
   content: PropTypes.string,
   color: PropTypes.string,
 };
+
+CalcButton.defaultProps = {
+  content: '1',
+  color: 'white',
+};
+
 export default Calculator;
