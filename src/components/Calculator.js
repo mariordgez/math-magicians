690a90610d72d9b99a30bc336bba003405019f9e
mariordgez <mariordgez@hotmail.com>
2021-09-17 08:30:43 -0700

    Separated code to make it more readable

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,69 +1,34 @@
 import React from 'react';
 import '../App.css';
-import PropTypes from 'prop-types';
-import calculate from '../logic/calculate';
+import CalcButton from './CalcButton';
 
-const Calculator = () => {
-  let calculator = { total: 0, next: null, operation: null };
-  const total = (cal) => {
-    if (cal.total === null && cal.next !== null) return '';
-    if (cal.total == null) return '0';
-
-    return cal.total;
-  };
-  const calc = (e) => {
-    const result = document.getElementById('result');
-    console.log(calculator.total);
-    console.log(calculator.next);
-    console.log(calculator.operation);
-
-    console.log(e.target.innerText);
-    calculator = calculate(calculator, e.target.innerText);
-    result.innerHTML = total(calculator) + (calculator.operation || '') + (calculator.next || '');
-  };
-  const CalcButton = ({ color, content }) => (
-    <button onClick={calc} type="button" className={color}>
-      {content}
-    </button>
-  );
-
-  CalcButton.propTypes = {
-    content: PropTypes.string,
-    color: PropTypes.string,
-  };
-
-  CalcButton.defaultProps = {
-    content: '1',
-    color: 'white',
-  };
-  return (
-    <div className="container">
-      <div className="gray result">
-        <p className="result" id="result">
-          {calculator.total}
-        </p>
-      </div>
-      <CalcButton content="AC" onClick={calc} color="white calc-btn ac" />
-      <CalcButton content="+/-" onClick={calc} color="white calc-btn plusminus" />
-      <CalcButton content="%" onClick={calc} color="white calc-btn percent" />
-      <CalcButton content="÷" onClick={calc} color="orange calc-btn division" />
-      <CalcButton content="1" onClick={calc} color="white calc-btn one" />
-      <CalcButton content="2" onClick={calc} color="white calc-btn two" />
-      <CalcButton content="3" onClick={calc} color="white calc-btn three" />
-      <CalcButton content="x" onClick={calc} color="orange calc-btn multi" />
-      <CalcButton content="4" onClick={calc} color="white calc-btn four" />
-      <CalcButton content="5" onClick={calc} color="white calc-btn five" />
-      <CalcButton content="6" onClick={calc} color="white calc-btn six" />
-      <CalcButton content="-" onClick={calc} color="orange calc-btn minus" />
-      <CalcButton content="7" onClick={calc} color="white calc-btn seven" />
-      <CalcButton content="8" onClick={calc} color="white calc-btn eight" />
-      <CalcButton content="9" onClick={calc} color="white calc-btn nine" />
-      <CalcButton content="+" onClick={calc} color="orange calc-btn plus" />
-      <CalcButton content="0" onClick={calc} color="white calc-btn cero" />
-      <CalcButton content="." onClick={calc} color="white calc-btn dot" />
-      <CalcButton content="=" onClick={calc} color="orange calc-btn equal" />
+const Calculator = () => (
+  <div className="container">
+    <div className="gray result">
+      <p className="result" id="result">
+        0
+      </p>
     </div>
-  );
-};
+    <CalcButton content="AC" color="white calc-btn ac" />
+    <CalcButton content="+/-" color="white calc-btn plusminus" />
+    <CalcButton content="%" color="white calc-btn percent" />
+    <CalcButton content="÷" color="orange calc-btn division" />
+    <CalcButton content="1" color="white calc-btn one" />
+    <CalcButton content="2" color="white calc-btn two" />
+    <CalcButton content="3" color="white calc-btn three" />
+    <CalcButton content="x" color="orange calc-btn multi" />
+    <CalcButton content="4" color="white calc-btn four" />
+    <CalcButton content="5" color="white calc-btn five" />
+    <CalcButton content="6" color="white calc-btn six" />
+    <CalcButton content="-" color="orange calc-btn minus" />
+    <CalcButton content="7" color="white calc-btn seven" />
+    <CalcButton content="8" color="white calc-btn eight" />
+    <CalcButton content="9" color="white calc-btn nine" />
+    <CalcButton content="+" color="orange calc-btn plus" />
+    <CalcButton content="0" color="white calc-btn cero" />
+    <CalcButton content="." color="white calc-btn dot" />
+    <CalcButton content="=" color="orange calc-btn equal" />
+  </div>
+);
 
 export default Calculator;
