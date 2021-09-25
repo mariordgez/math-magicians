724de85f9fad25a2ae885d2041a6815aac66bc18
mariordgez <mariordgez@hotmail.com>
2021-09-24 17:28:04 -0700

    Add calculator tests

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,15 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Home from '../components/Home';
+import Calculator from '../components/Calculator';
 
-describe('Test Calculator Homepage', () => {
-  test('Home element id to exist', () => {
-    render(<Home />);
-    expect(screen.getByTestId('homepage')).toBeInTheDocument();
+describe('Test Calculator', () => {
+  test('Expect correct text to render', () => {
+    render(<Calculator />);
+    expect(screen.getByText('Lets do some math!')).toBeInTheDocument();
   });
-
-  test('Text element to exist', () => {
-    render(<Home />);
-    expect(screen.getByText('Welcome to our page!')).toBeInTheDocument();
+  test('Expect number buttons to exist', () => {
+    render(<Calculator />);
+    expect(screen.getByText('1')).toBeInTheDocument();
+    expect(screen.getByText('2')).toBeInTheDocument();
+    expect(screen.getByText('3')).toBeInTheDocument();
+    expect(screen.getByText('4')).toBeInTheDocument();
+    expect(screen.getByText('5')).toBeInTheDocument();
+    expect(screen.getByText('6')).toBeInTheDocument();
+    expect(screen.getByText('7')).toBeInTheDocument();
+    expect(screen.getByText('8')).toBeInTheDocument();
+    expect(screen.getByText('9')).toBeInTheDocument();
+  });
+  test('Expect symbol buttons to exist', () => {
+    render(<Calculator />);
+    expect(screen.getByText('+/-')).toBeInTheDocument();
+    expect(screen.getByText('AC')).toBeInTheDocument();
+    expect(screen.getByText('=')).toBeInTheDocument();
+    expect(screen.getByText('.')).toBeInTheDocument();
+    expect(screen.getByText('+')).toBeInTheDocument();
+    expect(screen.getByText('-')).toBeInTheDocument();
+    expect(screen.getByText('x')).toBeInTheDocument();
+    expect(screen.getByText('%')).toBeInTheDocument();
   });
 });
