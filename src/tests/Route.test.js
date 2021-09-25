6ddf70a86cd37921d69a17e97a7cf2ce7f57b988
mariordgez <mariordgez@hotmail.com>
2021-09-24 18:14:56 -0700

    Add routing tests

--- src/tests/Route.test.js
+++ src/tests/Route.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Route from '../components/Route';
 
@@ -16,5 +16,17 @@ describe('Test Routes', () => {
   test('Navbar elements should exist', () => {
     render(<Route />);
     expect(screen.getByText('Welcome to our page!')).toBeInTheDocument();
+  });
+
+  test('Calculator should display', () => {
+    render(<Route />);
+    fireEvent.click(screen.getByText('Calculator'));
+    expect(screen.getByText('Lets do some math!')).toBeInTheDocument();
+  });
+
+  test('Quote page should display', () => {
+    render(<Route />);
+    fireEvent.click(screen.getByText('Quote'));
+    expect(screen.getByTestId('quotepage')).toBeInTheDocument();
   });
 });
