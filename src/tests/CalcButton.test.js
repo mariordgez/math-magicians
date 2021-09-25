dd00324106712c516a4d337bc303c59dd2c634a0
mariordgez <mariordgez@hotmail.com>
2021-09-25 02:20:54 -0700

    Fix test warning

--- src/tests/CalcButton.test.js
+++ src/tests/CalcButton.test.js
@@ -4,15 +4,15 @@ import CalcButton from '../components/CalcButton';
 
 describe('Test Calcbutton function', () => {
   test('AC symbol is returned by CalcButton', () => {
-    render(<CalcButton content="AC" color="white calc-btn ac" calc="ac" />);
+    render(<CalcButton content="AC" color="white calc-btn ac" />);
     expect(screen.getByText('AC')).toBeInTheDocument();
   });
   test('Equals symbol is returned by CalcButton', () => {
-    render(<CalcButton content="=" color="white calc-btn ac" calc="ac" />);
+    render(<CalcButton content="=" color="white calc-btn ac" />);
     expect(screen.getByText('=')).toBeInTheDocument();
   });
   test('Equals symbol class is returned by CalcButton', () => {
-    render(<CalcButton content="=" color="white calc-btn ac" calc="ac" />);
+    render(<CalcButton content="=" color="white calc-btn ac" />);
     expect(screen.getByText('=')).toHaveClass('white calc-btn ac');
   });
 });
